e4f6a8e52f98c28611cffb8111a71a8081959750
Gwenillia <55385809+Gwenillia@users.noreply.github.com>
2020-11-06 16:01:34 +0100

    update cards:hover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 //nav
 function toggleNav() {
-    const navToggler = document.getElementById("navLinks");
-    navToggler.classList.toggle("displayNav");
+  const navToggler = document.getElementById("navLinks");
+  navToggler.classList.toggle("displayNav");
 }
 
 const links = document.querySelectorAll(".nav__links");
 const sections = document.querySelectorAll("section");
 
 function linkState() {
-    let index = sections.length;
+  let index = sections.length;
 
-    while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
+  while (--index && window.scrollY + 100 < sections[index].offsetTop) {}
 
-    links.forEach((link) => link.classList.remove("active"));
-    links[index].classList.add("active");
+  links.forEach((link) => link.classList.remove("active"));
+  links[index].classList.add("active");
 }
 
 linkState();
@@ -21,26 +21,26 @@ window.addEventListener("scroll", linkState);
 
 // carousel
 function selector(e) {
-    const figs = document.getElementsByTagName("figure");
-    const slides = document.getElementsByClassName("slide");
+  const figs = document.getElementsByTagName("figure");
+  const slides = document.getElementsByClassName("slide");
 
-    for (i = 0; i < figs.length; i++) {
-        if (figs[i].classList.contains("visible")) {
-            figs[i].classList.remove("visible");
-            figs[i].classList.add("hidden");
-        } else {
-            figs[i].classList.add("visible");
-            figs[i].classList.remove("hidden");
-        }
+  for (i = 0; i < figs.length; i++) {
+    if (figs[i].classList.contains("visible")) {
+      figs[i].classList.remove("visible");
+      figs[i].classList.add("hidden");
+    } else {
+      figs[i].classList.add("visible");
+      figs[i].classList.remove("hidden");
     }
+  }
 
-    for (i = 0; i < slides.length; i++) {
-        if (slides[i].classList.contains("visible")) {
-            slides[i].classList.remove("visible");
-            slides[i].classList.add("hidden");
-        } else {
-            slides[i].classList.add("visible");
-            slides[i].classList.remove("hidden");
-        }
+  for (i = 0; i < slides.length; i++) {
+    if (slides[i].classList.contains("visible")) {
+      slides[i].classList.remove("visible");
+      slides[i].classList.add("hidden");
+    } else {
+      slides[i].classList.add("visible");
+      slides[i].classList.remove("hidden");
     }
+  }
 }
